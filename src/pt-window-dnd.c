9f3673c1e0480c42c180d1a9a9ab66fe27ac8dfd
Gabor Karsay <gabor.karsay@gmx.at>
2016-01-23 11:27:27 +0100

    [window] finish drag and drop handling

--- src/pt-window-dnd.c
+++ src/pt-window-dnd.c
@@ -26,43 +26,138 @@ static gboolean
 handle_timestamp (PtWindow *win,
 		  gchar *data)
 {
-	gboolean success;
-	
-	success = FALSE;
-	
-	/* TODO 
-	   Is it a timestamp? 
-	   If yes: jump to position */
+	/* Timestamp can have a short or long format:
+	          h:mm:ss-digit
+	   short:   00:00-0
+	   long:  0:00:00-0
 
-	return success;
+	   h can have several digits, m and s have always 2 digits in long format.
+	   m can have 1 or 2 digits in short format.
+	   Additionally it may be surrounded by #, e.g. #00:00-0#   */
+
+	gint     h, m, s, digit, pos;
+	gchar  **split;
+	gchar   *tmp = NULL;
+
+	if (g_regex_match_simple ("^#?[0-9][0-9]?:[0-9][0-9]-[0-9]#?$", data, 0, 0)) {
+		/* this is a short format, we add 0 for hours,
+		   we want only one delimiter, either # or : */
+		if (g_str_has_prefix (data, "#")) {
+			tmp = g_strdup_printf ("0%s", data);
+		} else {
+			tmp = g_strdup_printf ("0:%s", data);
+		}
+	}
+
+	if (g_regex_match_simple ("^#?[0-9]+:[0-9][0-9]:[0-9][0-9]-[0-9]#?$", data, 0, 0)) {
+		tmp = g_strdup (data);
+	}
+
+	if (!tmp)
+		return FALSE;
+
+	split = g_strsplit_set (tmp, "#:-", -1);
+
+	h = g_ascii_strtoll (split[0], NULL, 0);
+	m = g_ascii_strtoll (split[1], NULL, 0);
+	s = g_ascii_strtoll (split[2], NULL, 0);
+	digit = g_ascii_strtoll (split[3], NULL, 0);
+
+	g_strfreev (split);
+	g_free (tmp);
+
+	if (s > 59 || m > 59)
+		return FALSE;
+
+	pos = ((h * 3600 + m * 60 + s) * 10 + digit ) * 100;
+	pt_player_jump_to_position (win->priv->player, pos);
+
+	return TRUE;
 }
 
 static gboolean
 handle_uri (PtWindow *win,
 	    gchar *data)
 {
-	gboolean  success;
-	
-	success = FALSE;
+	GFile   *file;
+	gchar  **split = NULL;
+	gchar   *uri;
+	gboolean success = FALSE;
 
-	/* TODO 
-	   Is it a  valid uri or path? 
-	   If yes: open file */
+	if (gtk_widget_is_sensitive (win->priv->button_play)) {
+		/* We don't want to close any open files */
+		return success;
+	}
 
-	return success;
+	if (g_regex_match_simple ("^file:///.*", data, 0, 0)) {
+
+		/* Nautilus gives us a \r\n separated list */
+		split = g_strsplit (data, "\r", 0);
+		file = g_file_new_for_uri (split[0]);
+
+		if (g_file_query_exists (file, NULL)) {
+			pt_window_open_file (win, split[0]);
+			success = TRUE;
+		}
+
+		g_object_unref (file);
+		g_strfreev (split);
+
+		return success;
+	}
+
+
+	if (g_regex_match_simple ("^/.*/.*", data, 0, 0)) {
+
+		file = g_file_new_for_path (data);
+
+		if (g_file_query_exists (file, NULL)) {
+			uri = g_file_get_uri (file);
+			pt_window_open_file (win, uri);
+			g_free (uri);
+			success = TRUE;
+		}
+
+		g_object_unref (file);
+
+		return success;
+	}
+
+	return FALSE;
 }
 
 static gboolean
 handle_filename (PtWindow *win,
 		 gchar *data)
 {
-	gboolean success;
-	
+	gboolean       success;
+	GList	      *list;
+	GtkRecentInfo *info;
+	gchar	      *uri;
+
 	success = FALSE;
-	
-	/* TODO 
-	   Is it a filename in the recent files list? 
-	   If yes: open file */
+
+	list = gtk_recent_manager_get_items (win->priv->recent);
+	for (list = g_list_first (list); list; list = g_list_next (list)) {
+		info = (GtkRecentInfo *) list->data;
+		if (g_strcmp0 (gtk_recent_info_get_display_name (info), data) == 0) {
+			uri = g_strdup (gtk_recent_info_get_uri (info));
+			success = TRUE;
+			break;
+		}
+	}
+	g_list_free_full (list, (GDestroyNotify) gtk_recent_info_unref);
+
+	if (success) {
+		/* GStreamer has problems with uris from recent chooser,
+		   as a workaround use GFile magic */
+		GFile *file = g_file_new_for_uri (uri);
+		gchar *tmp = g_file_get_uri (file);
+		pt_window_open_file (PT_WINDOW (win), tmp);
+		g_free (tmp);
+		g_free (uri);
+		g_object_unref (file);
+	}
 
 	return success;
 }
