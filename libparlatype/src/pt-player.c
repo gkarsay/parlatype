9e706d56b7ecfedf17c90342fdf22279ca91ce2d
Gabor Karsay <gabor.karsay@gmx.at>
2016-12-04 15:43:47 +0100

    [player] update docs

--- libparlatype/src/pt-player.c
+++ libparlatype/src/pt-player.c
@@ -954,8 +954,13 @@ pt_player_wave_pos (PtPlayer *player)
 /**
  * pt_player_get_data:
  * @player: a #PtPlayer
+ * @pps: the requested pixel per second ratio
  *
- * Returns all data for visual representation.
+ * Returns wave data needed for visual representation as raw data. The
+ * requested resolution is given as pixel per seconds, e.g. 100 means one second
+ * is represented by 100 samples, is 100 pixels wide. The returned resolution
+ * doesn't have to be necessarily exactly the requested resolution, it might be
+ * a bit differnt, depending on the bit rate.
  *
  * Return value: (transfer full): the #PtWavedata
  */
