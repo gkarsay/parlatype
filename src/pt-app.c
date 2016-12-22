0c266c13ca85934f8f27591b887e9ab1c366c9ff
Gabor Karsay <gabor.karsay@gmx.at>
2016-12-22 12:05:02 +0100

    [window] use custom css, initially for goto cursor button

--- src/pt-app.c
+++ src/pt-app.c
@@ -315,6 +315,20 @@ pt_app_startup (GApplication *app)
 	}
 
 	g_object_unref (builder);
+
+	/* Load custom css */
+	GtkCssProvider *provider;
+	GFile          *file;
+	file = g_file_new_for_uri ("resource:///org/gnome/parlatype/parlatype.css");
+	provider = gtk_css_provider_new ();
+	/* Note: since 3.16 there's also gtk_css_provider_load_from_resource () */
+	gtk_css_provider_load_from_file (provider, file, NULL);
+	gtk_style_context_add_provider_for_screen (
+			gdk_screen_get_default (),
+			GTK_STYLE_PROVIDER (provider),
+			GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+	g_object_unref (provider);
+	g_object_unref (file);
 }
 
 static void
