529d854255836716bbce0101164e10b5414dced7
Gabor Karsay <gabor.karsay@gmx.at>
2018-08-25 14:53:58 +0200

    Update copyright years

--- src/pt-window.c
+++ src/pt-window.c
@@ -1,4 +1,4 @@
-/* Copyright (C) Gabor Karsay 2016 <gabor.karsay@gmx.at>
+/* Copyright (C) Gabor Karsay 2016-2018 <gabor.karsay@gmx.at>
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as published
