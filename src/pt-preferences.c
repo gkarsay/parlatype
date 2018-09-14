82914b1ee955f0b14c9d618c556464838ca2d8ac
Gabor Karsay <gabor.karsay@gmx.at>
2018-09-14 14:24:29 +0200

    [window] add preferences tab for timestamps

--- src/pt-preferences.c
+++ src/pt-preferences.c
@@ -24,27 +24,44 @@
 #ifdef GDK_WINDOWING_WAYLAND
 #include <gdk/gdkwayland.h>
 #endif
+#include <pt-player.h>
 #include "pt-preferences.h"
 
+#define EXAMPLE_TIME_SHORT 471123
+#define EXAMPLE_TIME_LONG 4071123
 
 static GtkWidget *preferences_dialog = NULL;
 
 struct _PtPreferencesDialogPrivate
 {
 	GSettings *editor;
+	PtPlayer  *player;
 
+	/* Waveform tab */
+	GtkWidget *pps_scale;
+	GtkWidget *ruler_check;
+	GtkWidget *fixed_cursor_radio;
+
+	/* Controls tab */
 	GtkWidget *spin_pause;
 	GtkWidget *spin_back;
 	GtkWidget *spin_forward;
-	GtkWidget *pps_scale;
 	GtkWidget *label_pause;
 	GtkWidget *label_back;
 	GtkWidget *label_forward;
+
+	/* Window tab */
 	GtkWidget *size_check;
 	GtkWidget *pos_check;
 	GtkWidget *top_check;
-	GtkWidget *ruler_check;
-	GtkWidget *fixed_cursor_radio;
+
+	/* Timestamps tab */
+	GtkWidget *precision_combo;
+	GtkWidget *separator_combo;
+	GtkWidget *delimiter_combo;
+	GtkWidget *hours_check;
+	GtkWidget *label_example1;
+	GtkWidget *label_example2;
 };
 
 G_DEFINE_TYPE_WITH_PRIVATE (PtPreferencesDialog, pt_preferences_dialog, GTK_TYPE_DIALOG)
@@ -116,10 +133,67 @@ set_pps (const GValue       *value,
 }
 
 static void
+update_example_timestamps (PtPreferencesDialog *dlg)
+{
+	gchar *example1;
+	gchar *example2;
+
+	example1 = pt_player_get_timestamp_for_time (dlg->priv->player, EXAMPLE_TIME_SHORT, EXAMPLE_TIME_SHORT);
+	example2 = pt_player_get_timestamp_for_time (dlg->priv->player, EXAMPLE_TIME_LONG, EXAMPLE_TIME_LONG);
+	gtk_label_set_text (GTK_LABEL (dlg->priv->label_example1), example1);
+	gtk_label_set_text (GTK_LABEL (dlg->priv->label_example2), example2);
+	g_free (example1);
+	g_free (example2);
+}
+
+void
+hours_check_toggled (GtkToggleButton *check,
+                     PtPreferencesDialog *dlg)
+{
+	g_settings_set_boolean (
+			dlg->priv->editor, "timestamp-fixed",
+			gtk_toggle_button_get_active (check));
+	update_example_timestamps (dlg);
+}
+
+void
+delimiter_combo_changed (GtkComboBox         *widget,
+                         PtPreferencesDialog *dlg)
+{
+	g_settings_set_string (
+			dlg->priv->editor, "timestamp-delimiter",
+			gtk_combo_box_get_active_id (widget));
+	update_example_timestamps (dlg);
+}
+
+void
+separator_combo_changed (GtkComboBox         *widget,
+                         PtPreferencesDialog *dlg)
+{
+	g_settings_set_string (
+			dlg->priv->editor, "timestamp-fraction-sep",
+			gtk_combo_box_get_active_id (widget));
+	update_example_timestamps (dlg);
+}
+
+void
+precision_combo_changed (GtkComboBox         *widget,
+                         PtPreferencesDialog *dlg)
+{
+	gint precision = gtk_combo_box_get_active (widget);
+	g_settings_set_int (
+			dlg->priv->editor, "timestamp-precision",
+			precision);
+	gtk_widget_set_sensitive (dlg->priv->separator_combo, (precision == 0) ? FALSE : TRUE);
+	update_example_timestamps (dlg);
+}
+
+static void
 pt_preferences_dialog_init (PtPreferencesDialog *dlg)
 {
 	dlg->priv = pt_preferences_dialog_get_instance_private (dlg);
 	dlg->priv->editor = g_settings_new ("com.github.gkarsay.parlatype");
+	dlg->priv->player = pt_player_new (NULL); /* no error handling, already checked in main window */
 
 	gtk_widget_init_template (GTK_WIDGET (dlg));
 	g_settings_bind (
@@ -187,6 +261,45 @@ pt_preferences_dialog_init (PtPreferencesDialog *dlg)
 	spin_back_changed_cb (GTK_SPIN_BUTTON (dlg->priv->spin_back), dlg);
 	spin_forward_changed_cb (GTK_SPIN_BUTTON (dlg->priv->spin_forward), dlg);
 	spin_pause_changed_cb (GTK_SPIN_BUTTON (dlg->priv->spin_pause), dlg);
+
+	/* Bind settings to our player instance for example timestamps */
+	g_settings_bind (
+			dlg->priv->editor, "timestamp-precision",
+			dlg->priv->player, "timestamp-precision",
+			G_SETTINGS_BIND_GET);
+
+	g_settings_bind (
+			dlg->priv->editor, "timestamp-fixed",
+			dlg->priv->player, "timestamp-fixed",
+			G_SETTINGS_BIND_GET);
+
+	g_settings_bind (
+			dlg->priv->editor, "timestamp-delimiter",
+			dlg->priv->player, "timestamp-delimiter",
+			G_SETTINGS_BIND_GET);
+
+	g_settings_bind (
+			dlg->priv->editor, "timestamp-fraction-sep",
+			dlg->priv->player, "timestamp-fraction-sep",
+			G_SETTINGS_BIND_GET);
+
+	/* setup timestamp fields according to settings */
+	gchar *delimiter = g_settings_get_string (dlg->priv->editor, "timestamp-delimiter");
+	if (!gtk_combo_box_set_active_id (GTK_COMBO_BOX (dlg->priv->delimiter_combo), delimiter)) {
+		gtk_combo_box_set_active_id (GTK_COMBO_BOX (dlg->priv->delimiter_combo), "#");
+	}
+	gchar *sep = g_settings_get_string (dlg->priv->editor, "timestamp-fraction-sep");
+	if (!gtk_combo_box_set_active_id (GTK_COMBO_BOX (dlg->priv->separator_combo), sep)) {
+		gtk_combo_box_set_active_id (GTK_COMBO_BOX (dlg->priv->separator_combo), ".");
+	}
+	gint precision = g_settings_get_int (dlg->priv->editor, "timestamp-precision");
+	if (precision < 0 || precision > 2)
+		precision = 1;
+	gtk_combo_box_set_active (GTK_COMBO_BOX (dlg->priv->precision_combo), precision);
+	gboolean fixed = g_settings_get_boolean (dlg->priv->editor, "timestamp-fixed");
+	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (dlg->priv->hours_check), fixed);
+	g_free (delimiter);
+	g_free (sep);
 }
 
 static void
@@ -195,6 +308,7 @@ pt_preferences_dialog_dispose (GObject *object)
 	PtPreferencesDialog *dlg = PT_PREFERENCES_DIALOG (object);
 
 	g_clear_object (&dlg->priv->editor);
+	g_clear_object (&dlg->priv->player);
 
 	G_OBJECT_CLASS (pt_preferences_dialog_parent_class)->dispose (object);
 }
@@ -221,6 +335,12 @@ pt_preferences_dialog_class_init (PtPreferencesDialogClass *klass)
 	gtk_widget_class_bind_template_child_private (widget_class, PtPreferencesDialog, label_forward);
 	gtk_widget_class_bind_template_child_private (widget_class, PtPreferencesDialog, ruler_check);
 	gtk_widget_class_bind_template_child_private (widget_class, PtPreferencesDialog, fixed_cursor_radio);
+	gtk_widget_class_bind_template_child_private (widget_class, PtPreferencesDialog, precision_combo);
+	gtk_widget_class_bind_template_child_private (widget_class, PtPreferencesDialog, separator_combo);
+	gtk_widget_class_bind_template_child_private (widget_class, PtPreferencesDialog, delimiter_combo);
+	gtk_widget_class_bind_template_child_private (widget_class, PtPreferencesDialog, hours_check);
+	gtk_widget_class_bind_template_child_private (widget_class, PtPreferencesDialog, label_example1);
+	gtk_widget_class_bind_template_child_private (widget_class, PtPreferencesDialog, label_example2);
 }
 
 void
