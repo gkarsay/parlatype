347634e20b33faa54a655ff95e8e0d7d55f52060
Gabor Karsay <gabor.karsay@gmx.at>
2019-04-21 19:35:15 +0200

    [docs] update namespace version in python example

--- libparlatype/docs/reference/parlatype-example.py
+++ libparlatype/docs/reference/parlatype-example.py
@@ -6,7 +6,7 @@
 import os
 import sys
 import gi
-gi.require_version('Parlatype', '1.0')
+gi.require_version('Parlatype', '2.0')
 gi.require_version('Gtk', '3.0')
 from gi.repository import Parlatype as Pt
 from gi.repository import Gtk
