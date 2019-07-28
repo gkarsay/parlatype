5c6269b5e9a2c5fa46f8cc5a896cb227af4713c5
Gabor Karsay <gabor.karsay@gmx.at>
2019-07-28 19:57:13 +0200

    [window] add new PT_CONTROLLER_TYPE

---
+++ src/pt-controller.c
@@ -0,0 +1,116 @@
+/* Copyright (C) Gabor Karsay 2019 <gabor.karsay@gmx.at>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include "config.h"
+#include "pt-window.h"
+#include "pt-controller.h"
+
+struct _PtControllerPrivate
+{
+	PtWindow   *win;
+};
+
+enum
+{
+	PROP_0,
+	PROP_WIN,
+	N_PROPERTIES
+};
+
+static GParamSpec *obj_properties[N_PROPERTIES] = { NULL, };
+
+G_DEFINE_TYPE_WITH_PRIVATE (PtController, pt_controller, G_TYPE_OBJECT)
+
+PtWindow*
+pt_controller_get_window (PtController *self)
+{
+	return self->priv->win;
+}
+
+PtPlayer*
+pt_controller_get_player (PtController *self)
+{
+	return self->priv->win->player;
+}
+
+static void
+pt_controller_init (PtController *self)
+{
+	self->priv = pt_controller_get_instance_private (self);
+}
+
+static void
+pt_controller_set_property (GObject      *object,
+                            guint         property_id,
+                            const GValue *value,
+                            GParamSpec   *pspec)
+{
+	PtController *self = PT_CONTROLLER (object);
+
+	switch (property_id) {
+	case PROP_WIN:
+		self->priv->win = g_value_get_object (value);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+		break;
+	}
+}
+
+static void
+pt_controller_get_property (GObject    *object,
+                            guint       property_id,
+                            GValue     *value,
+                            GParamSpec *pspec)
+{
+	PtController *self = PT_CONTROLLER (object);
+
+	switch (property_id) {
+	case PROP_WIN:
+		g_value_set_object (value, self->priv->win);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+		break;
+	}
+}
+static void
+pt_controller_class_init (PtControllerClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+	object_class->set_property = pt_controller_set_property;
+	object_class->get_property = pt_controller_get_property;
+
+	obj_properties[PROP_WIN] =
+	g_param_spec_object ("win",
+                             "Parent PtWindow",
+                             "Parent PtWindow",
+                             PT_WINDOW_TYPE,
+                             G_PARAM_READWRITE | G_PARAM_CONSTRUCT | G_PARAM_EXPLICIT_NOTIFY);
+
+	g_object_class_install_properties (
+			object_class,
+			N_PROPERTIES,
+			obj_properties);
+}
+
+PtController *
+pt_controller_new (PtWindow *win)
+{
+	return g_object_new (PT_CONTROLLER_TYPE, "win", win, NULL);
+}
---
+++ src/pt-controller.h
@@ -0,0 +1,53 @@
+/* Copyright (C) Gabor Karsay 2019 <gabor.karsay@gmx.at>
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PT_CONTROLLER_H
+#define PT_CONTROLLER_H
+
+#include "config.h"
+#include "pt-window.h"
+
+#define PT_CONTROLLER_TYPE		(pt_controller_get_type())
+#define PT_CONTROLLER(obj)		(G_TYPE_CHECK_INSTANCE_CAST((obj), PT_CONTROLLER_TYPE, PtController))
+#define PT_IS_CONTROLLER(obj)		(G_TYPE_CHECK_INSTANCE_TYPE ((obj), PT_CONTROLLER_TYPE))
+
+typedef struct _PtController		PtController;
+typedef struct _PtControllerClass	PtControllerClass;
+typedef struct _PtControllerPrivate	PtControllerPrivate;
+
+struct _PtController
+{
+	GObject parent;
+
+	/*< private > */
+	PtControllerPrivate *priv;
+};
+
+struct _PtControllerClass
+{
+	GObjectClass parent_class;
+};
+
+
+GType		pt_controller_get_type		(void) G_GNUC_CONST;
+
+PtWindow	*pt_controller_get_window	(PtController *self);
+
+PtPlayer	*pt_controller_get_player	(PtController *self);
+
+PtController 	*pt_controller_new		(PtWindow *win);
+
+#endif
