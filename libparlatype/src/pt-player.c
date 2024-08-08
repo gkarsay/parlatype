gkarsay/parlatype

commit dd874ac997009a221c81029790ce7c21d781c2a2

PtPlayer: connect to playbin signals later
Instead of in init, connect in constructed. The user data is the
PtPlayer instance, however, playbin can emit signals early, even before
PtPlayer is fully constructed.

libparlatype/src/pt-player.c
@@ -2150,6 +2150,26 @@ pt_player_get_mode (PtPlayer *player)
 }
 
 static void
+pt_player_constructed (GObject *object)
+{
+  PtPlayer *player = PT_PLAYER (object);
+  PtPlayerPrivate *priv = player->priv;
+
+  /* This is responsible for syncing system volume with Parlatype volume.
+     Syncing is done only in Play state */
+  g_signal_connect (G_OBJECT (priv->play),
+                    "notify::volume", G_CALLBACK (vol_changed), player);
+  g_signal_connect (G_OBJECT (priv->play),
+                    "notify::mute", G_CALLBACK (mute_changed), player);
+
+  /* Forward current-uri changes */
+  g_signal_connect (G_OBJECT (priv->play),
+                    "notify::current-uri", G_CALLBACK (uri_changed), player);
+
+  G_OBJECT_CLASS (pt_player_parent_class)->constructed (object);
+}
+
+static void
 pt_player_dispose (GObject *object)
 {
   PtPlayer *player = PT_PLAYER (object);
@@ -2373,17 +2393,6 @@ pt_player_init (PtPlayer *player)
                 "audio-filter", priv->scaletempo,
                 "audio-sink", priv->audio_bin, NULL);
 
-  /* This is responsible for syncing system volume with Parlatype volume.
-     Syncing is done only in Play state */
-  g_signal_connect (G_OBJECT (priv->play),
-                    "notify::volume", G_CALLBACK (vol_changed), player);
-  g_signal_connect (G_OBJECT (priv->play),
-                    "notify::mute", G_CALLBACK (mute_changed), player);
-
-  /* Forward current-uri changes */
-  g_signal_connect (G_OBJECT (priv->play),
-                    "notify::current-uri", G_CALLBACK (uri_changed), player);
-
   player->priv->current_state = GST_STATE_NULL;
   player->priv->target_state = GST_STATE_NULL;
 }
@@ -2393,6 +2402,7 @@ pt_player_class_init (PtPlayerClass *klass)
 {
   G_OBJECT_CLASS (klass)->set_property = pt_player_set_property;
   G_OBJECT_CLASS (klass)->get_property = pt_player_get_property;
+  G_OBJECT_CLASS (klass)->dispose = pt_player_constructed;
   G_OBJECT_CLASS (klass)->dispose = pt_player_dispose;
   G_OBJECT_CLASS (klass)->finalize = pt_player_finalize;
 

commit 959e7a99db498f4c9d965059b2671e2b293ad6cd

PtPlayer: fix syncing volume
Fix syncing volume when set with external tools like pavucontrol.
This regression was introduced in dd874ac.

Because of an awkward copy & paste error pt_player_constructed() was
never executed.

Also change 3 uses of g_idle_add() to g_idle_add_once(). Track their
handle IDs to clear them on dispose because one of them (volume) would
lead to a crash during tests.

libparlatype/src/pt-player.c
@@ -94,6 +94,9 @@ struct _PtPlayerPrivate
   GstClockTime segend;
 
   GCancellable *c;
+  guint         vol_changed_id;
+  guint         mute_changed_id;
+  guint         uri_changed_id;
 
   PtPrecisionType timestamp_precision;
   gboolean        timestamp_fixed;
@@ -2047,12 +2050,10 @@ pt_player_goto_timestamp (PtPlayer *self,
 
 /* --------------------- Init and GObject management ------------------------ */
 
-static gboolean
-notify_volume_idle_cb (PtPlayer *self)
+static void
+notify_volume_idle_cb (gpointer user_data)
 {
-  if (!PT_IS_PLAYER (self))
-    return G_SOURCE_REMOVE;
-
+  PtPlayer        *self = PT_PLAYER (user_data);
   PtPlayerPrivate *priv = pt_player_get_instance_private (self);
   gdouble          vol;
 
@@ -2065,7 +2066,7 @@ notify_volume_idle_cb (PtPlayer *self)
                                 obj_properties[PROP_VOLUME]);
     }
 
-  return FALSE;
+  priv->vol_changed_id = 0;
 }
 
 static void
@@ -2076,17 +2077,15 @@ vol_changed (GObject    *object,
   /* This is taken from Totem’s bacon-video-widget.c
      Changing the property immediately will crash, it has to be an idle source */
 
-  guint id;
-  id = g_idle_add ((GSourceFunc) notify_volume_idle_cb, self);
-  g_source_set_name_by_id (id, "[parlatype] notify_volume_idle_cb");
+  PtPlayerPrivate *priv = pt_player_get_instance_private (self);
+  priv->vol_changed_id = g_idle_add_once (notify_volume_idle_cb, self);
+  g_source_set_name_by_id (priv->vol_changed_id, "[parlatype] notify_volume_idle_cb");
 }
 
-static gboolean
-notify_mute_idle_cb (PtPlayer *self)
+static void
+notify_mute_idle_cb (gpointer user_data)
 {
-  if (!PT_IS_PLAYER (self))
-    return G_SOURCE_REMOVE;
-
+  PtPlayer        *self = PT_PLAYER (user_data);
   PtPlayerPrivate *priv = pt_player_get_instance_private (self);
   gboolean         mute;
 
@@ -2099,7 +2098,7 @@ notify_mute_idle_cb (PtPlayer *self)
                                 obj_properties[PROP_MUTE]);
     }
 
-  return FALSE;
+  priv->mute_changed_id = 0;
 }
 
 static void
@@ -2107,17 +2106,20 @@ mute_changed (GObject    *object,
               GParamSpec *pspec,
               PtPlayer   *self)
 {
-  guint id;
-  id = g_idle_add ((GSourceFunc) notify_mute_idle_cb, self);
-  g_source_set_name_by_id (id, "[parlatype] notify_mute_idle_cb");
+  PtPlayerPrivate *priv = pt_player_get_instance_private (self);
+
+  priv->mute_changed_id = g_idle_add_once (notify_mute_idle_cb, self);
+  g_source_set_name_by_id (priv->mute_changed_id, "[parlatype] notify_mute_idle_cb");
 }
 
-static gboolean
-notify_uri_idle_cb (PtPlayer *self)
+static void
+notify_uri_idle_cb (gpointer user_data)
 {
+  PtPlayer        *self = PT_PLAYER (user_data);
+  PtPlayerPrivate *priv = pt_player_get_instance_private (self);
   g_object_notify_by_pspec (G_OBJECT (self),
                             obj_properties[PROP_CURRENT_URI]);
-  return FALSE;
+  priv->uri_changed_id = 0;
 }
 
 static void
@@ -2125,9 +2127,10 @@ uri_changed (GObject    *object,
              GParamSpec *pspec,
              PtPlayer   *self)
 {
-  guint id;
-  id = g_idle_add ((GSourceFunc) notify_uri_idle_cb, self);
-  g_source_set_name_by_id (id, "[parlatype] notify_uri_idle_cb");
+  PtPlayerPrivate *priv = pt_player_get_instance_private (self);
+
+  priv->uri_changed_id = g_idle_add_once (notify_uri_idle_cb, self);
+  g_source_set_name_by_id (priv->uri_changed_id, "[parlatype] notify_uri_idle_cb");
 }
 
 /**
@@ -2311,6 +2314,9 @@ pt_player_dispose (GObject *object)
 
   remove_seek_source (self);
   g_clear_signal_handler (&priv->stream_notify_id, priv->collection);
+  g_clear_handle_id (&priv->vol_changed_id, g_source_remove);
+  g_clear_handle_id (&priv->mute_changed_id, g_source_remove);
+  g_clear_handle_id (&priv->uri_changed_id, g_source_remove);
   g_clear_pointer (&priv->collection, gst_object_unref);
   g_clear_object (&priv->info);
 
@@ -2537,7 +2543,7 @@ pt_player_class_init (PtPlayerClass *klass)
 {
   G_OBJECT_CLASS (klass)->set_property = pt_player_set_property;
   G_OBJECT_CLASS (klass)->get_property = pt_player_get_property;
-  G_OBJECT_CLASS (klass)->dispose = pt_player_constructed;
+  G_OBJECT_CLASS (klass)->constructed = pt_player_constructed;
   G_OBJECT_CLASS (klass)->dispose = pt_player_dispose;
   G_OBJECT_CLASS (klass)->finalize = pt_player_finalize;
 